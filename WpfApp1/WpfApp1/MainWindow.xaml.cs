1716454206d1fdd22b45db42cdff8bb4600c6544
Berkin Kağan Ateş <99278101+berkinKagan@users.noreply.github.com>
2023-08-17 19:39:01 +0300

    New PlanRem form's UI almost done!

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -28,6 +28,36 @@ namespace WpfApp1
             
         }
 
-        
+        private void newButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox planBox = new TextBox();
+            //Margin="60" Width="600" Height="60"
+            planBox.Margin = new Thickness(50);
+            planBox.Width = 500;
+            planBox.Height = 60;
+            planBox.TextWrapping = TextWrapping.Wrap;
+            newGrid.Children.Add(planBox);
+            newGrid.Visibility = Visibility.Visible;
+
+
+        }
+
+        private void oldsButton_Click(object sender, RoutedEventArgs e)
+        {
+            newGrid.Visibility = Visibility.Collapsed;
+        }
+    }
+
+    public class PlanRem
+    {
+        String text;
+        DateTime date;
+        String title;
+        public PlanRem(String text, DateTime date, String title)
+        {
+            this.text = text;
+            this.date = date;
+            this.title = title;
+        }
     }
 }
